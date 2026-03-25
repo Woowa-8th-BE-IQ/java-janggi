7e4a87b1cddff497ab66bbe8f292a229d192b9b1
dongheeHan <ark182818@gmail.com>
2026-03-25 13:13:35 +0900

    feat: 범위에 벗어난 열 좌표 생성 시 에러 발생 기능 구현
    - 범위는 1부터 9이다

--- src/main/java/janggi/domain/Row.java
+++ src/main/java/janggi/domain/Row.java
@@ -2,10 +2,20 @@ package janggi.domain;
 
 public class Row {
 
+    public static final int ROW_LOWER_THRESH_HOLD = 1;
+    public static final int ROW_UPPER_THRESH_HOLD = 9;
+
     private final int value;
 
     public Row(int value) {
+        validateRange(value);
         this.value = value;
+    }
+
+    private void validateRange(int value) {
+        if (value < ROW_LOWER_THRESH_HOLD || value > ROW_UPPER_THRESH_HOLD) {
+            throw new IllegalArgumentException("[ERROR] 열 좌표는 1~9까지 사용 가능 합니다");
+        }
     }
 
     public int getValue() {
--- src/test/java/janggi/domain/RowTest.java
+++ src/test/java/janggi/domain/RowTest.java
@@ -1,6 +1,8 @@
 package janggi.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import org.junit.jupiter.api.Test;
 
@@ -22,5 +24,16 @@ public class RowTest {
         assertThat(rowValue).isEqualTo(9);
     }
 
+    @Test
+    void 범위_1에서9_사이가_아닌_열좌표로_열을_생성하면_에러가_발생한다() {
 
+        assertAll(
+                () -> assertThatThrownBy(() -> new Row(0))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining("[ERROR] 열 좌표는 1~9까지 사용 가능 합니다"),
+                () -> assertThatThrownBy(() -> new Row(10))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining("[ERROR] 열 좌표는 1~9까지 사용 가능 합니다")
+        );
+    }
 }
