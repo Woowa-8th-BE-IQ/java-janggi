39aa0cf4cd7dd01cb94cd298752e510630cf33c2
dongheeHan <ark182818@gmail.com>
2026-03-25 13:22:44 +0900

    feat: 행 좌표 생성 및 예외 처리 추가

---
+++ src/main/java/janggi/domain/Column.java
@@ -0,0 +1,24 @@
+package janggi.domain;
+
+public class Column {
+
+    public static final int COLUMN_LOWER_THRESH_HOLD = 1;
+    public static final int COLUMN_UPPER_THRESH_HOLD = 10;
+
+    private final int value;
+
+    public Column(int value) {
+        validateRange(value);
+        this.value = value;
+    }
+
+    private void validateRange(int value) {
+        if (value < COLUMN_LOWER_THRESH_HOLD || value > COLUMN_UPPER_THRESH_HOLD) {
+            throw new IllegalArgumentException("[ERROR] 행 좌표는 1~10까지 사용 가능 합니다");
+        }
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
---
+++ src/test/java/janggi/domain/ColumnTest.java
@@ -0,0 +1,39 @@
+package janggi.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import org.junit.jupiter.api.Test;
+
+public class ColumnTest {
+
+    @Test
+    void 행좌표_1로_행을_생성하면_좌표값_1을_가지고있다() {
+        Column column = new Column(1);
+        int columnValue = column.getValue();
+
+        assertThat(columnValue).isEqualTo(1);
+    }
+
+    @Test
+    void 행좌표_10로_행을_생성하면_좌표값_10을_가지고있다() {
+        Column column = new Column(10);
+        int columnValue = column.getValue();
+
+        assertThat(columnValue).isEqualTo(10);
+    }
+
+    @Test
+    void 범위_1에서10_사이가_아닌_행좌표로_행을_생성하면_에러가_발생한다() {
+
+        assertAll(
+                () -> assertThatThrownBy(() -> new Column(0))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining("[ERROR] 행 좌표는 1~10까지 사용 가능 합니다"),
+                () -> assertThatThrownBy(() -> new Column(11))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining("[ERROR] 행 좌표는 1~10까지 사용 가능 합니다")
+        );
+    }
+}
