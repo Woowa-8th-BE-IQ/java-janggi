ab51c1c2b7ed39c89882db5f55d8a5f23bb20fce
dongheeHan <ark182818@gmail.com>
2026-03-26 22:17:04 +0900

    feat: 장기 이동 기능 구현

--- src/main/java/janggi/domain/Board.java
+++ src/main/java/janggi/domain/Board.java
@@ -1,5 +1,7 @@
 package janggi.domain;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class Board {
@@ -10,11 +12,21 @@ public class Board {
         this.board = board;
     }
 
-    public Map<Position, Piece> showBoard() {
-        return Map.copyOf(board);
+    public Map<Position, Piece> move(Position from, Position to) {
+        Piece fromPiece = board.get(from);
+        List<Position> path = fromPiece.getPath(from, to);
+        List<Piece> pieceOnPath = new ArrayList<>();
+        for (Position position : path) {
+            pieceOnPath.add(board.get(position));
+        }
+        if (fromPiece.canMove(pieceOnPath, board.get(to))) {
+            Piece removePiece = board.remove(from);
+            board.put(to, removePiece);
+        }
+        return showBoard();
     }
 
-    public Map<Position, Piece> move(Position from, Position to) {
-        return null;
+    public Map<Position, Piece> showBoard() {
+        return Map.copyOf(board);
     }
 }
