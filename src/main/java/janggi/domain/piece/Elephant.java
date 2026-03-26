d74114c7a3afdc33b5c111aebfd311f6bb1a3f42
dongheeHan <ark182818@gmail.com>
2026-03-26 21:40:34 +0900

    feat: 상 이동 규칙 예외 처리 구현

--- src/main/java/janggi/domain/piece/Elephant.java
+++ src/main/java/janggi/domain/piece/Elephant.java
@@ -3,6 +3,7 @@ package janggi.domain.piece;
 import janggi.domain.Piece;
 import janggi.domain.Position;
 import janggi.domain.Team;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
@@ -41,12 +42,34 @@ public class Elephant implements Piece {
 
     @Override
     public List<Position> getPath(Position from, Position to) {
-        return null;
+        if (!from.hasOffsetPairs(to, 2, 3)) {
+            throw new IllegalArgumentException("[ERROR] 상은 해당 경로로 이동할 수 없습니다.");
+        }
+
+        List<Position> path = new ArrayList<>();
+        Position next = from.moveStraight(to);
+        path.add(next);
+        path.add(next.moveDiagonal(to));
+        return path;
     }
 
     @Override
     public boolean canMove(List<Piece> piecesOnPath, Piece endPiece) {
+        validateAllPieceEmpty(piecesOnPath);
+        validateSameTeam(endPiece);
         return false;
+    }
+
+    private void validateAllPieceEmpty(List<Piece> piecesOnPath) {
+        if (!piecesOnPath.stream().allMatch(Piece::isEmptyPiece)) {
+            throw new IllegalArgumentException("[ERROR] 상의 이동 경로에 기물이 있을 수 없습니다.");
+        }
+    }
+
+    private void validateSameTeam(Piece endPiece) {
+        if (isSameTeam(endPiece)) {
+            throw new IllegalArgumentException("[ERROR] 자신의 기물로 이동할 수 없습니다.");
+        }
     }
 
     @Override
--- src/test/java/janggi/domain/piece/ElephantTest.java
+++ src/test/java/janggi/domain/piece/ElephantTest.java
@@ -1,8 +1,12 @@
 package janggi.domain.piece;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
+import janggi.domain.Position;
 import janggi.domain.Team;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class ElephantTest {
@@ -33,5 +37,46 @@ public class ElephantTest {
 
         String displayName = elephant.getDisplayName();
         assertThat(displayName).isEqualTo("상");
+    }
+
+    @Test
+    void 직선으로_먼저_한_칸_직선_방향의_대각선으로_연속_두_칸_이동시키면_경로를_반환한다() {
+        Elephant elephant = new Elephant(Team.HAN);
+
+        List<Position> path = elephant.getPath(Position.from("13"), Position.from("45"));
+
+        assertThat(path).containsExactly(Position.from("23"), Position.from("34"));
+    }
+
+    @Test
+    void 직선으로_먼저_한_칸_직선_방향의_대각선으로_연속_두_칸_이외의_경로로_이동시키면_예외가_발생한다() {
+        Elephant elephant = new Elephant(Team.HAN);
+
+        assertAll(
+                () -> assertThatThrownBy(() -> elephant.getPath(Position.from("35"), Position.from("65")))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage("[ERROR] 상은 해당 경로로 이동할 수 없습니다."),
+                () -> assertThatThrownBy(() -> elephant.getPath(Position.from("11"), Position.from("33")))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage("[ERROR] 상은 해당 경로로 이동할 수 없습니다.")
+        );
+    }
+
+    @Test
+    void 경로에_존재하는_기물_중_빈_기물이_아닌_기물이_있으면_예외가_발생한다() {
+        Elephant elephant = new Elephant(Team.HAN);
+
+        assertThatThrownBy(() -> elephant.canMove(List.of(new EmptyPiece(), new Soldier(Team.HAN)), new EmptyPiece()))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 상의 이동 경로에 기물이 있을 수 없습니다.");
+    }
+
+    @Test
+    void 이동할_위치에_같은_팀이_있으면_예외가_발생한다() {
+        Elephant elephant = new Elephant(Team.HAN);
+
+        assertThatThrownBy(() -> elephant.canMove(List.of(new EmptyPiece(), new EmptyPiece()), new Soldier(Team.HAN)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 자신의 기물로 이동할 수 없습니다.");
     }
 }
